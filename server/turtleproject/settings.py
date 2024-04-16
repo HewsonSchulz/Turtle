e4366c62a22064dd17dc7e7757b7616af69403ea
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-16 13:24:52 -0500

    removed unnecessary git tracking

--- server/turtleproject/settings.py
+++ server/turtleproject/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,4 +131,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
