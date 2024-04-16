f4c9d9a9a96a2082dbb2334aecaa839d67bb9865
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-16 14:15:39 -0500

    username verification

--- server/turtleapi/views/register.py
+++ server/turtleapi/views/register.py
@@ -1,7 +1,7 @@
 '''Register user'''
 
 import json
-from django.http import HttpResponse, HttpResponseNotAllowed
+from django.http import HttpResponseNotAllowed, JsonResponse
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
@@ -14,23 +14,35 @@ from rest_framework.authtoken.models import Token
 def register_user(request):
     '''user creation'''
 
-    req_body = json.loads(request.body.decode())
+    if request.method == 'POST':
 
-    new_user = User.objects.create_user(
-        username=req_body['username'],
-        email=req_body['email'],
-        password=req_body['password'],
-        first_name=req_body['first_name'],
-        last_name=req_body['last_name'],
-    )
+        req_body = json.loads(request.body)
 
-    token = Token.objects.create(user=new_user)
+        if User.objects.filter(username=req_body['username']).exists():
+            # username is taken
+            return JsonResponse(
+                {'valid': False, 'message': 'That username is taken'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-    # Return the token to the client
-    data = json.dumps({'token': token.key, 'id': new_user.id})
-    return HttpResponse(
-        data, content_type='application/json', status=status.HTTP_201_CREATED
-    )
+        # create new user
+        new_user = User.objects.create_user(
+            username=req_body['username'],
+            email=req_body['email'],
+            password=req_body['password'],
+            first_name=req_body['first_name'],
+            last_name=req_body['last_name'],
+        )
+
+        # create new token
+        token = Token.objects.create(user=new_user)
+
+        return JsonResponse(
+            {'valid': True, 'token': token.key, 'id': new_user.id},
+            status=status.HTTP_201_CREATED,
+        )
+    else:
+        return HttpResponseNotAllowed(['POST'])
 
 
 @csrf_exempt
@@ -47,14 +59,14 @@ def login_user(request):
         auth_user = authenticate(username=name, password=pwd)
 
         if auth_user:
+            # user exists
             auth_user.last_login = timezone.now()
             auth_user.save()
 
             token = Token.objects.get(user=auth_user)
-            data = json.dumps({'valid': True, 'token': token.key, 'id': auth_user.id})
-            return HttpResponse(data, content_type='application/json')
+            return JsonResponse({'valid': True, 'token': token.key, 'id': auth_user.id})
         else:
-            data = json.dumps({'valid': False})
-            return HttpResponse(data, content_type='application/json')
+            # user does not exist
+            return JsonResponse({'valid': False}, status=status.HTTP_400_BAD_REQUEST)
 
-    return HttpResponseNotAllowed(permitted_methods=['POST'])
+    return HttpResponseNotAllowed(['POST'])
