c7d4e0ca110cbb0c22b9475f46aeb7a7a0ea1b25
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-18 10:44:13 -0500

    base refactoring

--- server/turtleapi/models/custard.py
+++ server/turtleapi/models/custard.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Custard(models.Model):
     flavor = models.CharField(max_length=255)
+    base = models.ForeignKey('CustardBase', on_delete=models.CASCADE)
     image_path = models.CharField(max_length=255, null=True, blank=True)
     toppings = models.ManyToManyField(
         'Topping', through='CustardTopping', related_name='custards'
