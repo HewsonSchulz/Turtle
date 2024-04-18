fa80178d177d07ab1867faced41d1190b5780e2e
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-18 08:55:22 -0500

    toppings fixture

--- server/seed_data.sh
+++ server/seed_data.sh
@@ -4,4 +4,4 @@ rm db.sqlite3
 rm -rf ./turtleapi/migrations
 python3 manage.py makemigrations turtleapi
 python3 manage.py migrate
-python3 manage.py loaddata users tokens ranks positions custards employees
+python3 manage.py loaddata users tokens ranks positions toppings custards employees
