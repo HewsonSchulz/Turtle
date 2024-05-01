82c4399f365bc57b3af3a0dd82ac6fd5a6890ceb
Hewson Schulz <hewsonschulz@gmail.com>
2024-05-01 09:29:09 -0500

    🪲💥🔫

--- server/turtleapi/models/employee.py
+++ server/turtleapi/models/employee.py
@@ -11,14 +11,14 @@ class Employee(models.Model):
     date_employed = models.DateTimeField(null=True)
     date_unemployed = models.DateTimeField(null=True, blank=True)
     fav_position = models.ForeignKey(
-        'Position', on_delete=models.CASCADE, related_name='employees', null=True
+        'Position', on_delete=models.SET_NULL, related_name='employees', null=True
     )
     fav_food = models.ForeignKey(
-        'MenuItem', on_delete=models.CASCADE, related_name='employees', null=True
+        'MenuItem', on_delete=models.SET_NULL, related_name='employees', null=True
     )
     fav_custard = models.ForeignKey(
-        'Custard', on_delete=models.CASCADE, related_name='employees', null=True
+        'Custard', on_delete=models.SET_NULL, related_name='employees', null=True
     )
     rank = models.ForeignKey(
-        'Rank', on_delete=models.CASCADE, related_name='employees', null=True
+        'Rank', on_delete=models.SET_NULL, related_name='employees', null=True
     )
