988dc2c59eabb0b6965b4c0d2a835508a92653b8
Hewson Schulz <hewsonschulz@gmail.com>
2024-05-05 00:39:28 -0500

    admin employee label

--- client/src/components/user/UsersList.js
+++ client/src/components/user/UsersList.js
@@ -14,7 +14,9 @@ export const UsersList = () => {
       <div className='users-list__employees'>
         {employees?.map((employee) => (
           <div key={employee.id} className='users-list__employee'>
-            <div className='users-list__employee-username'>{employee.username}</div>
+            <div className='users-list__employee-username'>
+              {employee.username} {employee.is_admin && <i className='users-list__employee-admin'>ADMIN</i>}
+            </div>
             <div className='users-list__employee-date-joined'>Joined: {formatDate(employee.date_joined)}</div>
             <div className='users-list__employee-last-login'>Last login: {formatDate(employee.last_login)}</div>
           </div>
