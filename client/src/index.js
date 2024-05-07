dcda70b7127ac41da1404d685cf571d00e6cb3b8
Hewson Schulz <hewsonschulz@gmail.com>
2024-05-07 14:24:41 -0500

    changed BrowserRouter to HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import { App } from './App'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -13,10 +13,10 @@ const root = createRoot(container)
 const queryClient = new QueryClient()
 
 root.render(
-  <BrowserRouter basename='/Turtle'>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <App />
       {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
